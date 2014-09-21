e52a8045924b6a0368d6abbb1ca055f824c5fc0b
willsherwood <will.sherwood5@gmail.com>
2014-09-20 22:42:57 -0500

    Added audio support

---
+++ src/sherwood/audio/Audio.java
@@ -0,0 +1,30 @@
+package sherwood.audio;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Audio {
+
+	public synchronized void play(String SOUND_FILENAME) {
+		try {
+			URL url = this.getClass().getClassLoader()
+					.getResource(SOUND_FILENAME);
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioIn);
+			clip.start();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+}
